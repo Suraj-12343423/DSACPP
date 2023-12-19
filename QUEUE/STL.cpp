404451d3920822da74f66afdeb983453462c56e2
Sai Suraj <surajkalla809@gmail.com>
2023-12-19 16:27:04 +0530

    QUEUE

---
+++ QUEUE/STL.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+	
+	queue<int> q;
+	
+	q.push(2);
+	q.push(3);
+	q.push(5);
+		
+	cout<<"size of queue = "<<q.size()<<endl;
+	
+	cout<<"Front of queue is= "<<q.front()<<endl;
+	
+	
+	
+	
+	
+	
+	return 0;
+}
